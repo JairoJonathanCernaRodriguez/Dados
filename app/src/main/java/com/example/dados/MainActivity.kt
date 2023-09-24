f17b03cb2e87e796935e44c1ea6f20b08b683397
JairoJonathanCernaRodriguez <jairocergtav@gmail.com>
2023-09-24 16:20:59 -0600

    Initial commit

---
+++ app/src/main/java/com/example/dados/MainActivity.kt
@@ -0,0 +1,89 @@
+package com.example.dados
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.dados.ui.theme.DadosTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            DadosTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    DiceRollerApp()
+                }
+            }
+        }
+    }
+}
+@Preview
+@Composable
+fun DiceRollerApp() {
+    DiceWithButtonAndImage(modifier = Modifier
+        .fillMaxSize()
+        .wrapContentSize(Alignment.Center)
+
+    )
+
+}
+val l = @Composable(){
+
+}
+@Composable
+fun DiceWithButtonAndImage(modifier: Modifier = Modifier) {
+    var result by remember{ mutableStateOf(5) }
+    var re2 by remember{ mutableStateOf(5) }
+
+    Column(modifier = modifier, horizontalAlignment = Alignment.CenterHorizontally){
+        getRandomDiceImage(result)
+        getRandomDiceImage(re2)
+
+        Spacer(modifier = Modifier.height(16.dp));
+        Button(onClick = { result = (1..6).random()
+            re2 = (1..6).random()
+        })
+        {
+            Text(text = stringResource(R.string.btn_roller))
+        }
+    }
+}
+@Composable
+fun getRandomDiceImage(imagen:Int) {
+    val imagesResource2 = when(imagen){
+        1 -> R.drawable.dice_1
+        2 -> R.drawable.dice_2
+        3 -> R.drawable.dice_3
+        4 -> R.drawable.dice_4
+        5 -> R.drawable.dice_5
+        else -> R.drawable.dice_6
+
+    }
+    Image(painter = painterResource(id = imagesResource2), contentDescription = stringResource(
+        R.string.image_cara_2
+    ) )
+}
